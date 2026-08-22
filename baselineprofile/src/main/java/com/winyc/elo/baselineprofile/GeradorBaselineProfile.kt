c2517eb9ccd5b5f2de797f9bcd222339a7dc080d
Winycios nascimento <winycios.alves@infotera.com.br>
2026-08-21 22:34:01 -0300

    .

--- baselineprofile/src/main/java/com/winyc/elo/baselineprofile/GeradorBaselineProfile.kt
+++ baselineprofile/src/main/java/com/winyc/elo/baselineprofile/GeradorBaselineProfile.kt
@@ -4,7 +4,7 @@ import androidx.benchmark.macro.junit4.BaselineProfileRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWit
+import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class GeradorBaselineProfile {
